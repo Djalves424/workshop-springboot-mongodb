44697cb320c685612fcd8aea1df54a58b82a3e5f
Diego Alves <djalves424@gmail.com>
2023-05-20 00:18:12 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/diegoalves/resources/UserResource.java
+++ src/main/java/com/diegoalves/resources/UserResource.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.diegoalves.domain.Post;
 import com.diegoalves.domain.User;
 import com.diegoalves.dto.UserDTO;
 import com.diegoalves.services.UserService;
@@ -58,12 +59,19 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-//	@Update(value="/{id}")
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	@PutMapping(value="/{id}")
+//	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
  	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
 		User obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 		}
+	
+	@GetMapping(value="/{id}/posts")
+//	@RequestMapping(value="/{id/posts}", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
+}
