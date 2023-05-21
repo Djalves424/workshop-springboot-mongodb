d7e550f41dbd045b44f9308224456ac889d2bf79
Diego Alves <djalves424@gmail.com>
2023-05-20 21:46:38 -0300

    Obtendo um post por id

---
+++ src/main/java/com/diegoalves/services/PostService.java
@@ -0,0 +1,27 @@
+package com.diegoalves.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegoalves.domain.Post;
+import com.diegoalves.repository.PostRepository;
+import com.diegoalves.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
