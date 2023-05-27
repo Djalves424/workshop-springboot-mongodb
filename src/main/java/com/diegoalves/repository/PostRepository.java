6f26287a83b07fb17a46c6e98bd719fd1f383473
Diego Alves <djalves424@gmail.com>
2023-05-26 22:25:45 -0300

    Consulta simples com query methods

--- src/main/java/com/diegoalves/repository/PostRepository.java
+++ src/main/java/com/diegoalves/repository/PostRepository.java
@@ -1,9 +1,14 @@
 package com.diegoalves.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.diegoalves.domain.Post;
 
+@Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
