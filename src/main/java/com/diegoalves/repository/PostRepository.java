8275752f5e10782ce522f8f88daee8c97e8fafb3
Diego Alves <djalves424@gmail.com>
2023-05-26 23:23:52 -0300

    Consulta com vários critérios

--- src/main/java/com/diegoalves/repository/PostRepository.java
+++ src/main/java/com/diegoalves/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.diegoalves.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -11,8 +12,11 @@ import com.diegoalves.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
-	@Query("{ 'title'<field>: { $regex: ?0, $options: 'i' } }")
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: { $gte: ?1} }, { date: { $lte: ?2 } } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } }, {} ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date dateMaxDate);
 }
--- src/main/java/com/diegoalves/resources/PostResource.java
+++ src/main/java/com/diegoalves/resources/PostResource.java
@@ -1,10 +1,15 @@
 package com.diegoalves.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.diegoalves.domain.Post;
 import com.diegoalves.resources.util.URL;
@@ -29,6 +34,19 @@ public class PostResource {
  	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value="/{fullsearch}")
+//	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> fullSeacrh(
+ 			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.covertDate(minDate, new Date(0L));
+		Date max = URL.covertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
